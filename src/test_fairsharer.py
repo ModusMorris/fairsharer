681d77d836ae6199ace4b5267c21729ca109535e
Maurice <maurice.191999@gmail.com>
2023-12-02 19:12:43 +0100

    added code for my test_fairsharer but it doesnt work

---
+++ src/test_fairsharer.py
@@ -0,0 +1,13 @@
+import pytest
+from fairsharer import fair_sharer
+
+def test_fair_sharer():
+    # Test case 1
+    result = fair_sharer([0, 1000, 800, 0], 1)
+    assert result == [100, 800, 900, 0]
+
+    # Test case 2
+    result = fair_sharer([0, 1000, 800, 0], 2)
+    assert result == [100, 890, 720, 90]
+
+
